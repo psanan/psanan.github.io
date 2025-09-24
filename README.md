0193d05b6bbbe1980e3c66e24b0a7bc6a15d41a2
Patrick Sanan <patrick.sanan@gmail.com>
2025-09-24 21:11:33 +0200

    Update deployment for GitHub pages

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ It tries to avoid abstraction and complication beyond
 [basic HTML and CSS](https://developer.mozilla.org). It prioritizes
 simplicity.
 
-It's non-scalable. It will be "fun" iff it's small and simple.
+It's non-scalable. It's "fun" iff it's small and simple.
 It's only directly useful to me, though I hope it demonstrates
 that you can and should build your own site in a simple way.
 
@@ -16,13 +16,14 @@ resources are in subdirectories of `site/`.
 There is no "build"! Clone this repository and open `site/index.html` with a
 web browser. Edit the files in a text editor and reload the browser page.
 
-To deploy, upload the contents of `site/` (including hidden files) to your
-web server (probably to `$HOME/public_html`).
+You can deploy by uploading the contents of `site/` (including
+hidden files) to your web server (probably to `$HOME/public_html`). Currently,
+I'm deploying by pushing the `site/` subtree to a branch set up for GitHub
+Pages. See `deploy.sh`.
 
 Python scripts in `scripts/` (using only standard modules) help with feed generation
 and bulk formatting operations on the HTML.
 
-`deploy.sh`, for convenience, gives more hints.
 
 Scripts depend on some command line tools:
 
--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env sh
 
-# Note that the below relies on an SSH alias "webhost"
-# to define where to upload files.
-# See e.g. https://wiki.debian.org/SshAliases
-
-target_dir="public_html"
-
 printf -- "Updating HTML\n"
 python scripts/html_update.py
 return_value=$?
@@ -17,11 +11,27 @@ fi
 printf -- "Updating feed\n"
 python scripts/feed_update.py
 
-printf -- "Updating non-image files\n"
-# Images are big so don't update existing files in any "image" directories
-# If you want to wipe out some images to update, do it manually for now, like
-# ssh webhost  "rm -rf public_html/images/foo/"
-rsync -r --progress --delete --exclude=images/ site/ "webhost:$target_dir/"
+printf -- "Pushing branch for GitHub pages\n"
+# Deploy the site subtree, so the index is at the root as required
+# by Github pages.
+# There is no "force" option here.
+# If this update is rejected, delete the gh-pages
+# branch and reconfigure GitHub pages settings to publish
+# it to patricksanan.org.
+git subtree push --prefix site origin gh-pages
 
-printf -- "Uploading new image files\n"
-rsync -r --progress --delete --ignore-existing site/images/ "webhost:$target_dir/images/"
+# # Alternate to rsync files directly.
+# # This relies on an SSH alias "webhost"
+# # to define where to upload files.
+# # See e.g. https://wiki.debian.org/SshAliases
+#
+# target_dir="public_html"
+#
+# printf -- "Updating non-image files\n"
+# # Images are big so don't update existing files in any "image" directories
+# # If you want to wipe out some images to update, do it manually for now, like
+# # ssh webhost  "rm -rf public_html/images/foo/"
+# rsync -r --progress --delete --exclude=images/ site/ "webhost:$target_dir/"
+#
+# printf -- "Uploading new image files\n"
+# rsync -r --progress --delete --ignore-existing site/images/ "webhost:$target_dir/images/"
